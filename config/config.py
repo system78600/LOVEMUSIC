1b04fb665afc0eccc2414bfabac6b52ed1bc397d
system78600 <uzefkhatri552@gmail.com>
2026-03-14 22:11:40 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VIPBOLTE/LOVEMUSIC",
+    "https://github.com/system78600/LOVEMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
